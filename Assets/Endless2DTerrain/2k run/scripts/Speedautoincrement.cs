6c4222f7fdd0f6191b4a125c34297d3c9b0070a4
surya prakash <surya251992@gmail.com>
2017-01-12 18:17:55 +0530

    speed increment updated

--- Assets/Endless2DTerrain/2k run/scripts/Speedautoincrement.cs
+++ Assets/Endless2DTerrain/2k run/scripts/Speedautoincrement.cs
@@ -13,11 +13,10 @@ public class Speedautoincrement : MonoBehaviour {
 
 	IEnumerator Increment()
 	{
-		if(pl.speed < 41)
-		while(true)
+			while (pl.speed < 44) 
 		{
-			pl.speed = pl.speed + 1f;
-			yield return new WaitForSeconds (40.0f);
+				pl.speed = pl.speed + 1f;
+				yield return new WaitForSeconds (25.0f);
 		}
 	}
 	
